039148749b5d753c362d6807a17329f083348917
jungeun <pje8845@naver.com>
2020-11-07 02:43:33 +0900

    메뉴버튼함수, 각종 버튼 수정 시간표 관리화면, 과목추가팝업 수정

--- timetable/timetable_add.js
+++ timetable/timetable_add.js
@@ -14,12 +14,12 @@
 // }
 // 아래는 브라우저 창 크기 기준으로 중앙에 팝업 띄움
 function showTimetableAddPopup() {
-    var popupWidth = 600;
+    var popupWidth = 650;
     var popupHeight = 800;
     var popupX = (document.body.offsetWidth/2) - (popupWidth/2);
     var popupY = (document.body.offsetHeight/2) - (popupHeight/2);
-    var url = "timetable_edit.html";
-    var name = "timetable edit popup"
+    var url = "timetable_add.html";
+    var name = "timetable add popup"
     var option = "width ="+popupWidth+", height ="+popupHeight+", left"+popupX+", top="+popupY+", scrollbars = yes, location = no";
     window.open(url, name, option);
 }
