414714707e65ff6c76ebc9f10d43c19e97f777f2
jungeun <pje8845@naver.com>
2020-11-24 03:05:19 +0900

    로그인 기능 구현 중, 로그인시 메인화면에 프로필 적용

--- login/login.js
+++ login/login.js
@@ -7,8 +7,9 @@ function btnLoginHome() {
 function login() {
     var userId = doc.getElementById('login_id').value;
     var userPassword = doc.getElementById('login_password').value;
-    var id_ok = true;
-    var pass_ok = true;
+    var userName;
+    var id_ok = false;
+    var pass_ok = false;
 
     //입력 잘 되었나 확인용
     console.log(userId);
@@ -25,19 +26,34 @@ function login() {
         doc.getElementById('userPassword').focus(); //커서 가도록 함
         return;
     }
-
+    console.log(userId);
+    console.log(userPassword);
+    console.log('-------------');
     //회원정보와 일치하는지 확인
-    //in here
-    //
+    //for문으로 비교해서 로그인하는게 안됨....;
+    for (var key in userlist) {
+        console.log(this.userId);
+        // console.log(this.name);
+        if (this.userId == userId) { //id 일치 확인
+            id_ok = true;
+            alert('id는 잇음');
+            if (this.password === userPassword) { //pw 일치 확인
+                pass_ok = true;
+                userName = this.name; //사용자의 이름 저장
+                alert('둘다 잇음');
+                break;
+            }
+        }
+    }
     
-    if (id_ok==false) { //아이디 확인
+    if (id_ok===false) { //아이디 확인(아이디조차 존재하지 않는 경우)
         alert('아이디를 다시 확인해주세요');
         doc.getElementById('login_id').value=''; //아이디 지우기
         doc.getElementById('login_password').value=''; //비밀번호 지우기
         doc.getElementById('login_id').focus(); //아이디 커서
         return;
     }
-    if (pass_ok==false) { //해당 아이디의 비밀번호 확인
+    if (pass_ok===false) { //해당 아이디의 비밀번호 확인
         alert('비밀번호가 아이디와 일치하지 않습니다.');
         doc.getElementById('login_password').value=''; //비밀번호 지우기
         doc.getElementById('login_password').focus(); //아이디 커서
@@ -45,5 +61,9 @@ function login() {
     }
     
     //로그인 정상적으로 된 경우
-    location.replace("../mainframe.html");
+    //현재 로그인 된 계정 저장(activeUser)
+    activeUser.id = userId; //활성화된 사용자 아이디 저장
+    activeUser.name = userName; //활성화된 사용자 이름 저장
+
+    location.replace("/mainframe.html"); //메인 화면으로 이동
 }
---
+++ user/users.js
@@ -0,0 +1,25 @@
+var userlist = new Array(); //사용자들을 담을 배열
+var activeUser = new Object(); //로그인 중인 사용자를 담을 객체
+userlist.push(
+    {
+        userId : 'je991025@gmail.com',
+        password : 'angelje',
+        name : '박정은'
+        // course: { //과목 정보 담을 객체
+            
+        // }
+    }
+);
+
+
+function createUser(userid, userpw, username) { //userlist에 새로운 사용자 추가
+    var user = new Object();
+    user.userId = userid;
+    user.password = userpw;
+    user.name = username;
+    userlist.push(user);
+    //console.log('새로운사용자'+user.userId+user.name);
+    return true;
+}
+
+activeUser = userlist[0]; //test사용자를 로그인 시켜둠
