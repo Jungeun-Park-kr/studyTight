319885da899fe2bdbe90d86a10db954322b208a7
pje8845 <pje8845@naver.com>
2020-12-01 01:39:56 +0900

    메인화면 실시간 강의 이동기능 완료

--- mainframe.js
+++ mainframe.js
@@ -12,23 +12,81 @@ function plusTodo(){
         var text=window.prompt("오늘의 할일을 입력하세요","ex) 웹 공부하기");
         var wrap=document.getElementById('todolist');
         if(text!=null){
-               var new_todo=document.createElement("input");
-               new_todo.setAttribute("type","checkbox");
-               new_todo.setAttribute("id","new_todo");
-               new_todo.setAttribute("value",text);
+                var new_todo=document.createElement("input");
+                new_todo.setAttribute("type","checkbox");
+                new_todo.setAttribute("id","new_todo");
+                new_todo.setAttribute("value",text);
 
-               var new_label=document.createElement("label");
-               new_label.setAttribute('for','new_todo') //체크박스 아이디
-               wrap.appendChild(new_todo);
-               new_todo.appendChild(document.createTextNode(text))
-              
+                var new_label=document.createElement("label");
+                new_label.setAttribute('for','new_todo') //체크박스 아이디
+                wrap.appendChild(new_todo);
+                new_todo.appendChild(document.createTextNode(text))
+                
                         //일단 안된다 ,,
-        }
+}
 }
 function starClicked(){
         document.getElementById("star").src="/media/full_star.png";
-        //하나만 적용된다 ㄱ-
+//하나만 적용된다 ㄱ-
 }
 function newFolder(){
         alert("폴더 추가하기 버튼을 눌렀습니다");
+}
+
+function getRealtimeCourse(course) {
+        var type, tmptime, day, st, sh, sm;
+        var curday = 1; //테스트용 시간
+        var curhour = 12;
+        var curmin = 40; 
+        // var curday = getDay(); 
+        // var curhour = getHours();
+        // var curmin = getMinutes();
+        var cur = (curhour*3600)+(curmin*60); //시간 초단위로 변환
+        console.log(cur);
+        for(var i=0; i<course.time.length; i++) {
+                tmptime = course.time[i];
+                day = dayStrToNum(tmptime.day); //요일을 번호로 바꿔서 저장
+                console.log('day숫자:'+day);
+                st = tmptime.stime.split(':');
+                sh = Number(st[0]); 
+                sm = Number(st[1]);
+                console.log('sh,sm'+sh+sm);
+                if (day == curday) { //요일 비교
+                        console.log('요일동일');
+                        var cortime = (sh*3600)+(sm*60);
+                        console.log(course);
+                        var gap = cortime - cur; //남은시간 계산
+                        console.log('gap'+gap);
+                        if (gap >=0 && gap <= 1800) { //30분 이내에 수업이 있는 경우
+                                type = getCourseType(course.type, course.location);
+                                return (tmptime.stime+' '+course.title+' '+type); //시작시간 과목이름 과목종류 리턴
+                        }
+                }
+        }
+        return null;
+}
+
+function getCourseType(type, location) { //과목 타입(online_realtime,online_video,offline)
+        switch(type) {
+                case 'online_realtime':
+                        return "온라인 실시간";
+                case 'online_video':
+                        return "온라인 동영상";
+                case 'offline': //오프라인이면 강의실 리턴
+                        return location;
+        }
+}
+function dayStrToNum(day) {
+        switch(day) {
+                case 'mon':
+                        return 1;
+                case 'tue':
+                        return 2;
+                case 'wed':
+                        return 3;
+                case 'thu':
+                        return 4;
+                case 'fri':
+                        return 5;
+        }
 }
