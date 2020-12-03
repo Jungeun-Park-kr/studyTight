c2c0b3c3b59c4e725ec5445fc64b93768e6a451f
godwpfh <y64551@gmail.com>
2020-12-03 15:44:37 +0900

    d-day 내용 저장, delete구현

--- d-day/d-day.js
+++ d-day/d-day.js
@@ -9,38 +9,46 @@ function selectdate(){
 
     var dday=document.getElementById("d-date");
     dday.setAttribute('value',"D-"+gap)
+
+    return gap
 }
+
 function selectContent(){
-        var content=prompt("D-day 내용을 입력하세요","ex)웹 공부하기");
+        content=prompt("D-day 내용을 입력하세요","ex)웹 공부하기");
         dContent=document.getElementById("d-content")
         dContent.setAttribute('value',content)
+    return content
+    }      
+function storeDday(){
+//    var Dday=document.getElementById('.d-date');
+//    var Dcontent = document.getElementById('.d-content');
+
+//    var day=Dday.getAttribute(value);
+//    var content=Dcontent.getAttribute(value);
+
+    localStorage.setItem('d-date',selectdate());
+    localStorage.setItem('d-content',selectContent());
+
+    //저장완료. 근데 등록버튼을 먼저 눌러야 실행된다.
+
+    return plusDday(localStorage.getItem('d-date'),localStorage.getItem('d-content'));
+
 }
-function storeDday(day,content){
-    var Dday=document.getElementById('.d-day');
-    var Dcontent = document.getElementById('.d-content');
-
-    var day=Dday.getAttribute('value');
-    var content=Dcontent.getAttribute('value');
-
-    localStorage.setItem('d-day',day);
-    localStorage.setItem('d-content',content);
-
-    alert("저장되었습니다.");
+function plusDday(new_date, new_content){
+    
 }
 
 function modifyDday(dday){
 
 }
 function deleteDday(){
-        var eventTarget=document.getElementsByClassName('btn_delete_d_day');
+    var eventTarget=document.getElementsByClassName('btn_delete_d-day');
 
-        for(var i=0;i<eventTarget.length;i++){
-            eventTarget[i].addEventListener('click',function(){
-                var parent=document.querySelector('#table-d tbody');
-                parent.removeChild(this.parentElement);
-                i--;
-            })
-        }
+   // for(var i=0; i < eventTarget.length; i++){
+    //    var d=eventTarget[i];
+        var parent=document.getElementById('table_body');
+        parent.removeChild(eventTarget.parentElement.parentElement);
+        // i++;
+        //다 똑같은 d를 가질까.. 흠.. 삭제는 골라서는 안되고 전체가 삭제된다.
+       // }
 }
-//디데이 색 관련해서도 함수를 만들어야 할 듯?
-//mainframe에 보이는 디데이 관련해서
