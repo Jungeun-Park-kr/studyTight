f59cf1bad890758ef0cf85054018dc3858d60803
godwpfh <y64551@gmail.com>
2020-12-05 12:09:06 +0900

    폴더, 포스트잇 정렬

--- folder/pluspostit_file.js
+++ folder/pluspostit_file.js
@@ -1,5 +1,7 @@
 function uploadFile(){
-    var new_file=document.getElementById('input_file');
-
+    var new_file=document.getElementById('input_file'); 
+    var new_div=document.createElement('div');
+    new_div.setAttribute('class','new_postit_file');
+   // new_div.setAttribute('new'); 
     //파일 이름이 같으면 그 파일을 보여주기. 
 }
