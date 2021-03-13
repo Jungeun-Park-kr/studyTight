e113f66a5b06f63c2419422f161f9ad6ef6ed3aa
godwpfh <0112jyoung@naver.com>
2021-03-13 22:29:41 +0900

    folder내 추가하기 수정 중

---
+++ folder/new_folder.js
@@ -0,0 +1,23 @@
+function newPostIt(){
+    //파일, 내용 , 링크 저장하는 팝업창
+    var popupWidth = 400;
+    var popupHeight = 300;
+    var popupX = (document.body.offsetWidth/2) - (popupWidth/2);
+    var popupY = (document.body.offsetHeight/2) - (popupHeight/2);
+    var url = "new_choice.html"; 
+    var name = "plus postIt"
+    var option = "width ="+popupWidth+", height ="+popupHeight+", left="+popupX+", top="+popupY+", scrollbars = yes, location = no";
+    window.open(url, name, option);
+}
+function choice(index){
+    //0-2가 글, 링크, 파일 순으로 되어있음
+    if(index==0){
+
+    }
+    else if(index==1){
+
+    }
+    else{ //index==2
+
+    }
+}
