f966baafe72c8faaca1b21febd5c7345947b1ba5
godwpfh <0112jyoung@naver.com>
2021-03-20 16:42:36 +0900

    폴더 내 수정

--- folder/new_folder.js
+++ folder/new_folder.js
@@ -45,3 +45,41 @@ function choice(index){
         window.open(url, name, option);
     }
 }
+function showPostMenu(index){
+    //나중에 index로 받아서 처리하기
+    $(`#dropdown-content${index}`).show();
+    $(`#revise_post${index}`).text("수정하기");
+    $(`#revise_post${index}`).show();
+    $(`#delete_post${index}`).text("삭제하기");
+    $(`#delete_post${index}`).show();
+}
+
+function revisePostClicked(index){
+    alert("제목이나 글을 수정하였습니다.");
+}
+function deletePostClicked(index){
+    alert("포스트잇을 삭제하시겠습니까?");
+}
+
+// 타켓 영역 제외하고 클릭했을 때 숨김처리
+// $('body').on('click',function(e){
+//     var tgPoint=$(e.target);
+//     var popCallBtn=tgPoint.hasClass(`dropdown-content${index}`)
+    
+//     if(!popCallBtn){
+//         $(`dropdown-cotent${index}`).removeClass('view');
+//     }
+// });
+
+var star_toggle=1
+function starPostClicked(index){
+    var star=document.getElementsByClassName('fixed_star');
+    if(star_toggle%2==1){
+        star[index].src="/media/full_star.png"
+        star[index].style.opacity=1;
+    }else{
+        star[index].src="/media/empty_star.png"
+        // hover할 때만 있도록 설정해야 함.
+    }
+    star_toggle++;
+}
