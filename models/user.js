0468e3200adcec2380914a97d7c94dc04d38f3b3
JungEun Park <je991025@gmail.com>
2021-05-17 03:57:19 +0900

    feat : Add default profile image

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const path = require('path'); // 현재 프로젝트의 경로
 
 const {Schema} = mongoose;
 // const { Types: {ObjectId}} = Schema;
@@ -6,7 +7,9 @@ const userSchema = new Schema( {
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     name :{ type: String, required: true },
-    profile_image : { type: String, default: 'default_img'},
+    profile_image : { type: String,
+        default: '/public/media/user.png'
+    },
     // provider: { //로그인 로컬로
     //     type: String,
     //     default: 'local',
