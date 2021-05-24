bf94bb497732d6771d7f6e00073ae2d4c5afd3d4
JungEun Park <je991025@gmail.com>
2021-05-24 21:21:11 +0900

    feat : Add DB models for timetable

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('./user');
-
+const Timetable = require('./timetable');
 
 const uri = process.env.ATLAS_URI; // .env에 DB계정 잘 입력되었는지 확인
 const connect = () => {
