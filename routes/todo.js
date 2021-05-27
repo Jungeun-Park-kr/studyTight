71bf08c998d87717a62f0dcdbd5ae9d7213bfe8e
godwpfh <0112jyoung@naver.com>
2021-05-27 23:52:06 +0900

    Add router to patch, try to revise update todolist

--- routes/todo.js
+++ routes/todo.js
@@ -7,7 +7,7 @@ const fs=require('fs');
 
 router.post('/',isLoggedIn, async(req,res,next) => {
     var content=req.body.todo_content;
-    console.log(content);
+    // console.log(content);
     
     try{
     //몽고db에 저장
@@ -21,12 +21,36 @@ router.post('/',isLoggedIn, async(req,res,next) => {
     //로그인 된 유저 : console.log('로그인:'+req.user.email);
 
     res.render('../views/mainframe.ejs',
-        { title : 'study Tight'}
+        { title : 'study Tight', todolist:todo}
     );
+    //res.send(todo);
 }catch(err){
     next(err);
 }
 });
+
+router.patch('/:id',isLoggedIn, async(req,res,next) => {
+    try{
+    Todo.updateOne({
+        user_id:req.user._id
+    },{
+        $set:{
+            todo_finished:true
+        }
+    });
+    }catch(err){
+        next(arr);
+    } });
+    // try{ //이거는 todo_finished값이 바뀌었으니까
+    //     if(req.user._id){
+    //     //id가 이미 존재해야 하니까
+    //     const update_todo=await Todo.
+
+    //     }
+    // }catch(err){
+    //     next(arr);
+    // }
+
 module.exports=router;
 
 function getCurrentDate(){
