a9f2c225fbafdc824913ba1e27fb31bd457e8a82
godwpfh <0112jyoung@naver.com>
2021-05-29 19:47:56 +0900

    revise to show updated todolist, add day schema

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/', isLoggedIn, async(req, res) => { // app.get('주소', 라우터)
         res.render('../views/mainframe.ejs', {
             title: 'StudyTight 메인화면',
             todolist : todolist,
-            timetable : timetable,
+            timetable : timetable
         });
     }
     catch (err) {
@@ -31,6 +31,24 @@ router.get('/', isLoggedIn, async(req, res) => { // app.get('주소', 라우터)
     }
 });
 
+router.get('/todo', isLoggedIn, async(req, res) => { // app.get('주소', 라우터) : GET 요청이 올때 할 동작
+    try {
+        const todolist = await Todo.find({user_id: req.user._id}).populate('user_id');
+        const timetable = await Course.find({user_id: res.locals.user._id}).populate('user_id').populate('schedules').sort({'createdAt':-1});
+        //console.info(todolist);
+        // res.send('Hello, Express'); // 테스트용
+        res.render('../views/mainframe.ejs', {
+            title: 'StudyTight 메인화면',
+            todolist : todolist,
+            timetable : timetable
+        });
+    }
+    catch (err) {
+        console.error('routes/index.js 에서 에러');
+        console.error(err);
+        next(err);
+    }
+});
 router.get('/', isNotLoggedIn, (req, res) => {
     try {
         // res.send('Hello, Express'); // 테스트용
