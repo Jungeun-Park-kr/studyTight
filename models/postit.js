3b356e6f9aa9a116dd965689c21aa29074c52222
godwpfh <0112jyoung@naver.com>
2021-07-20 15:07:49 +0900

    Making modal of link, content

--- models/postit.js
+++ models/postit.js
@@ -7,7 +7,7 @@ const {Schema} = mongoose;
 const postItSchema = new Schema({
     postIt_name : {type: String, required:true},
     postIt_content: {type: String, required:true, unique:true},
-    postIt_type : {type: String, required:true, unique:false},
+    postIt_type : {type: String, required:true},
     postIt_star : {type: Boolean, required:true},
     postIt_color: {type:String, required: true}
 });
