f45239ff1826b3e43b551b96dbe133f8969af408
noino0819 <noino0819@naver.com>
2021-07-23 03:39:49 +0900

    profile edit

--- routes/guestbook/guestbook.js
+++ routes/guestbook/guestbook.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { isLoggedIn, isNotLoggedIn} = require('../middlewares');
+const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 const Profile = require('../../models/profile');
 const CourseSchedule = require('../../models/course_schedule');
 const router = express.Router();
@@ -13,30 +13,56 @@ router.use((req, res, next) => {
 
 // guestbook
 
-router.get('/', isLoggedIn, async (req, res, next) => {
+router.get('/', isLoggedIn, async(req, res, next) => {
 
     try {
-        
-        const profile = await Profile.find({user_id: res.locals.user._id}).populate('user_id').populate('schedules').sort({'createdAt':-1});
-        
-        res.render( '../views/guestbook/guestbook_myroom.ejs', {
-            // user : res.locals.user,
-            // school: school,
-            // school_private: school_privat,
-            // major: major,
-            // major_private : major_private,
-            // grade : grade,
-            // grade_private : grade_private,
-            // timetable_private : timetable_private,
-            // birth_private : birth_private
+
+        const profile = await Profile.find({ user_id: res.locals.user._id }).populate('user_id').populate('schedules').sort({ 'createdAt': -1 });
+
+        res.render('../views/guestbook/guestbook_myroom.ejs', {
+            school: req.body.school,
+            school_private: req.body.school_privat,
+            major: req.body.major,
+            major_private: req.body.major_private,
+            grade: req.body.grade,
+            grade_private: req.body.grade_private,
+            // timetable_private: timetable_private,
+            birth_private: req.body.birth_private
         });
 
-        
 
-    }
-    catch (err) {
+
+    } catch (err) {
         console.error('/views/timetable/guestbook_myroom.ejs 에서 에러');
         console.error(err);
+        next(err);
+    }
+});
+
+router.post('/', isLoggedIn, async(req, res, next) => {
+    var content = req.body.todo_content;
+
+
+    try {
+        //몽고db에 저장
+        const todo = await Todo.create({
+            user_id: req.user._id,
+            todo_content: content,
+            register_date: getCurrentDate(),
+            todo_finished: false
+        });
+
+        res.send({
+            _id: todo._id,
+            user_id: req.user._id,
+            todo_content: todo.todo_content,
+            todo_finished: todo.todo_finished
+        });
+        // res.render('../views/mainframe.ejs',
+        //     { title : 'study Tight', todolist:todo, timetable:timetable, folder: folder}
+        // );
+
+    } catch (err) {
         next(err);
     }
 });
