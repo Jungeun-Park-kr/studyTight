ca5fb8cd5a303c3b68b5a27432af165a9f4e8e3c
noino0819 <noino0819@naver.com>
2021-08-21 05:46:01 +0900

    front_friend

--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose'); //model의 구성요소에서 필요한것
 const { Schema } = mongoose;
+const { Types: { ObjectId } } = Schema;
 
 const profile_schema = new Schema({
     user_id: { // 과목 주인의 _id (이메일 아님, 몽고DB 자체 _id)
         type: Schema.Types.ObjectId,
-
         ref: 'User'
     },
     school: { type: String, required: true },
@@ -14,7 +14,9 @@ const profile_schema = new Schema({
     grade: { type: Number, required: true },
     grade_private: { type: Boolean },
     age_private: { type: Boolean },
-    timetable_private: { type: Boolean }
+    timetable_private: { type: Boolean },
+    group: [{ type: String }]
+
 });
 
 module.exports = mongoose.model('Profile', profile_schema);
